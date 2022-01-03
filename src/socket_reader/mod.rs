91fcffb496a74fb724d39c5887fa013775057f29
Andrey <amigin@gmail.com>
2022-01-03 17:26:10 +0200

    Payling with accesses

--- src/socket_reader/mod.rs
+++ src/socket_reader/mod.rs
@@ -2,11 +2,9 @@ mod error;
 mod read_buffer;
 mod socket_reader;
 
-#[cfg(test)]
 mod socket_reader_mock;
 pub use error::ReadingTcpContractFail;
 pub use read_buffer::ReadBuffer;
 pub use socket_reader::{SocketReader, SocketReaderTcpStream};
 
-#[cfg(test)]
 pub use socket_reader_mock::SocketReaderMock;
