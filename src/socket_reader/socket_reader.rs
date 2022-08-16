095e783a00dbaef2a25182a0ec4145229bfb7d98
amigin <amigin@gmail.com>
2022-08-17 00:43:53 +0300

    Extended SocketReader

--- src/socket_reader/socket_reader.rs
+++ src/socket_reader/socket_reader.rs
@@ -11,9 +11,11 @@ use super::{ReadBuffer, ReadingTcpContractFail};
 pub trait SocketReader {
     async fn read_byte(&mut self) -> Result<u8, ReadingTcpContractFail>;
     async fn read_i32(&mut self) -> Result<i32, ReadingTcpContractFail>;
+    async fn read_u32(&mut self) -> Result<u32, ReadingTcpContractFail>;
     async fn read_bool(&mut self) -> Result<bool, ReadingTcpContractFail>;
     async fn read_byte_array(&mut self) -> Result<Vec<u8>, ReadingTcpContractFail>;
     async fn read_i64(&mut self) -> Result<i64, ReadingTcpContractFail>;
+    async fn read_u64(&mut self) -> Result<u64, ReadingTcpContractFail>;
     async fn read_buf(&mut self, buf: &mut [u8]) -> Result<(), ReadingTcpContractFail>;
     async fn read_until_end_marker<'s>(
         &mut self,
@@ -82,6 +84,17 @@ impl SocketReader for SocketReaderTcpStream {
         Ok(i32::from_le_bytes(buf))
     }
 
+    async fn read_u32(&mut self) -> Result<u32, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 4;
+        let mut buf = [0u8; DATA_SIZE];
+
+        self.read_buf(&mut buf).await?;
+
+        self.read_size += DATA_SIZE;
+
+        Ok(u32::from_le_bytes(buf))
+    }
+
     async fn read_i64(&mut self) -> Result<i64, ReadingTcpContractFail> {
         const DATA_SIZE: usize = 8;
         let mut buf = [0u8; DATA_SIZE];
@@ -91,6 +104,17 @@ impl SocketReader for SocketReaderTcpStream {
         self.read_size += DATA_SIZE;
 
         Ok(i64::from_le_bytes(buf))
+    }
+
+    async fn read_u64(&mut self) -> Result<u64, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 8;
+        let mut buf = [0u8; DATA_SIZE];
+
+        self.read_buf(&mut buf).await?;
+
+        self.read_size += DATA_SIZE;
+
+        Ok(u64::from_le_bytes(buf))
     }
 
     async fn read_bool(&mut self) -> Result<bool, ReadingTcpContractFail> {
--- src/socket_reader/socket_reader_in_mem.rs
+++ src/socket_reader/socket_reader_in_mem.rs
@@ -35,6 +35,22 @@ impl SocketReader for SocketReaderInMem {
         Ok(result)
     }
 
+    async fn read_u32(&mut self) -> Result<u32, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 4;
+
+        let mut buf = [0u8; DATA_SIZE];
+
+        buf.copy_from_slice(&self.data[0..DATA_SIZE]);
+
+        let result = u32::from_le_bytes(buf);
+
+        for _ in 0..DATA_SIZE {
+            self.data.remove(0);
+        }
+
+        Ok(result)
+    }
+
     async fn read_bool(&mut self) -> Result<bool, ReadingTcpContractFail> {
         let result = self.read_byte().await?;
         Ok(result > 0u8)
@@ -65,6 +81,22 @@ impl SocketReader for SocketReaderInMem {
         buf.copy_from_slice(&self.data[0..DATA_SIZE]);
 
         let result = i64::from_le_bytes(buf);
+
+        for _ in 0..DATA_SIZE {
+            self.data.remove(0);
+        }
+
+        Ok(result)
+    }
+
+    async fn read_u64(&mut self) -> Result<u64, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 8;
+
+        let mut buf = [0u8; DATA_SIZE];
+
+        buf.copy_from_slice(&self.data[0..DATA_SIZE]);
+
+        let result = u64::from_le_bytes(buf);
 
         for _ in 0..DATA_SIZE {
             self.data.remove(0);
