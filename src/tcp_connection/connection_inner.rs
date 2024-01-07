992067a95163ee8a4cfcb5228fb2e9c7ecf015f7
amigin <amigin@gmail.com>
2024-01-08 00:56:19 +0200

    Debugging eventloop

--- src/tcp_connection/connection_inner.rs
+++ src/tcp_connection/connection_inner.rs
@@ -155,6 +155,7 @@ impl TcpConnectionInner {
 #[async_trait::async_trait]
 impl EventsLoopTick<()> for TcpConnectionInner {
     async fn started(&self) {
+        println!("EventsLoop started: {:?}", self.get_log_context().await);
         self.threads_statistics.increase_read_threads();
     }
 
@@ -163,6 +164,7 @@ impl EventsLoopTick<()> for TcpConnectionInner {
     }
 
     async fn finished(&self) {
+        println!("EventsLoop finished: {:?}", self.get_log_context().await);
         self.threads_statistics.decrease_read_threads();
     }
 }
