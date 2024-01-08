1a0d1984fd1f0257248a84826e38d8f558b23598
amigin <amigin@gmail.com>
2024-01-08 16:52:26 +0200

    TcpPayload is removed since we do not need it anymore

--- src/tcp_socket_serializer.rs
+++ src/tcp_socket_serializer.rs
@@ -5,44 +5,6 @@ use crate::{
     TcpWriteBuffer,
 };
 
-pub enum TcpPayload<'s> {
-    AsVec(Vec<u8>),
-    AsSlice(&'s [u8]),
-}
-
-impl<'s> TcpPayload<'s> {
-    pub fn as_slice(&'s self) -> &'s [u8] {
-        match self {
-            TcpPayload::AsVec(v) => v.as_slice(),
-            TcpPayload::AsSlice(s) => s,
-        }
-    }
-
-    pub fn into_vec(self) -> Vec<u8> {
-        match self {
-            TcpPayload::AsVec(v) => v,
-            TcpPayload::AsSlice(s) => s.to_vec(),
-        }
-    }
-}
-
-impl<'s> Into<TcpPayload<'s>> for &'s Vec<u8> {
-    fn into(self) -> TcpPayload<'s> {
-        TcpPayload::AsSlice(self)
-    }
-}
-
-impl<'s> Into<TcpPayload<'s>> for &'s [u8] {
-    fn into(self) -> TcpPayload<'s> {
-        TcpPayload::AsSlice(self)
-    }
-}
-impl<'s> Into<TcpPayload<'s>> for Vec<u8> {
-    fn into(self) -> TcpPayload<'s> {
-        TcpPayload::AsVec(self)
-    }
-}
-
 #[async_trait]
 pub trait TcpSocketSerializer<TContract: Send + Sync + 'static> {
     const PING_PACKET_IS_SINGLETON: bool;
