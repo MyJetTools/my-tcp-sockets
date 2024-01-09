c376d3fa861997685a65a6d0675836f6bf3344f6
amigin <amigin@gmail.com>
2024-01-09 10:45:18 +0200

    Fixes with serializer

--- src/socket_reader/socket_reader.rs
+++ src/socket_reader/socket_reader.rs
@@ -10,12 +10,54 @@ use super::{ReadBuffer, ReadingTcpContractFail};
 #[async_trait]
 pub trait SocketReader {
     async fn read_byte(&mut self) -> Result<u8, ReadingTcpContractFail>;
-    async fn read_i32(&mut self) -> Result<i32, ReadingTcpContractFail>;
-    async fn read_u32(&mut self) -> Result<u32, ReadingTcpContractFail>;
-    async fn read_bool(&mut self) -> Result<bool, ReadingTcpContractFail>;
-    async fn read_byte_array(&mut self) -> Result<Vec<u8>, ReadingTcpContractFail>;
-    async fn read_i64(&mut self) -> Result<i64, ReadingTcpContractFail>;
-    async fn read_u64(&mut self) -> Result<u64, ReadingTcpContractFail>;
+
+    async fn read_i32(&mut self) -> Result<i32, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 4;
+        let mut buf = [0u8; DATA_SIZE];
+        self.read_buf(&mut buf).await?;
+        Ok(i32::from_le_bytes(buf))
+    }
+
+    async fn read_u32(&mut self) -> Result<u32, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 4;
+        let mut buf = [0u8; DATA_SIZE];
+
+        self.read_buf(&mut buf).await?;
+        Ok(u32::from_le_bytes(buf))
+    }
+
+    async fn read_bool(&mut self) -> Result<bool, ReadingTcpContractFail> {
+        let b = self.read_byte().await?;
+
+        return Ok(b > 0);
+    }
+
+    async fn read_byte_array(&mut self) -> Result<Vec<u8>, ReadingTcpContractFail> {
+        let size = self.read_i32().await? as usize;
+
+        let mut value: Vec<u8> = Vec::with_capacity(size);
+        unsafe { value.set_len(size) }
+        self.read_buf(&mut value).await?;
+
+        return Ok(value);
+    }
+
+    async fn read_i64(&mut self) -> Result<i64, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 8;
+        let mut buf = [0u8; DATA_SIZE];
+
+        self.read_buf(&mut buf).await?;
+
+        Ok(i64::from_le_bytes(buf))
+    }
+
+    async fn read_u64(&mut self) -> Result<u64, ReadingTcpContractFail> {
+        const DATA_SIZE: usize = 8;
+        let mut buf = [0u8; DATA_SIZE];
+        self.read_buf(&mut buf).await?;
+        Ok(u64::from_le_bytes(buf))
+    }
+
     async fn read_buf(&mut self, buf: &mut [u8]) -> Result<(), ReadingTcpContractFail>;
     async fn read_until_end_marker<'s>(
         &mut self,
@@ -71,68 +113,6 @@ impl SocketReader for SocketReaderTcpStream {
         self.read_size += 1;
 
         return Ok(buf[0]);
-    }
-
-    async fn read_i32(&mut self) -> Result<i32, ReadingTcpContractFail> {
-        const DATA_SIZE: usize = 4;
-        let mut buf = [0u8; DATA_SIZE];
-
-        self.read_buf(&mut buf).await?;
-
-        self.read_size += DATA_SIZE;
-
-        Ok(i32::from_le_bytes(buf))
-    }
-
-    async fn read_u32(&mut self) -> Result<u32, ReadingTcpContractFail> {
-        const DATA_SIZE: usize = 4;
-        let mut buf = [0u8; DATA_SIZE];
-
-        self.read_buf(&mut buf).await?;
-
-        self.read_size += DATA_SIZE;
-
-        Ok(u32::from_le_bytes(buf))
-    }
-
-    async fn read_i64(&mut self) -> Result<i64, ReadingTcpContractFail> {
-        const DATA_SIZE: usize = 8;
-        let mut buf = [0u8; DATA_SIZE];
-
-        self.read_buf(&mut buf).await?;
-
-        self.read_size += DATA_SIZE;
-
-        Ok(i64::from_le_bytes(buf))
-    }
-
-    async fn read_u64(&mut self) -> Result<u64, ReadingTcpContractFail> {
-        const DATA_SIZE: usize = 8;
-        let mut buf = [0u8; DATA_SIZE];
-
-        self.read_buf(&mut buf).await?;
-
-        self.read_size += DATA_SIZE;
-
-        Ok(u64::from_le_bytes(buf))
-    }
-
-    async fn read_bool(&mut self) -> Result<bool, ReadingTcpContractFail> {
-        let b = self.read_byte().await?;
-
-        return Ok(b > 0);
-    }
-
-    async fn read_byte_array(&mut self) -> Result<Vec<u8>, ReadingTcpContractFail> {
-        let size = self.read_i32().await? as usize;
-
-        let mut value: Vec<u8> = Vec::with_capacity(size);
-        unsafe { value.set_len(size) }
-        self.read_buf(&mut value).await?;
-
-        self.read_size += size + 4;
-
-        return Ok(value);
     }
 
     async fn read_until_end_marker<'s>(
--- src/tcp_connection/mod.rs
+++ src/tcp_connection/mod.rs
@@ -1,6 +1,6 @@
-mod connection;
 mod connection_statistics;
-mod connection_stream;
+mod tcp_connection;
+mod tcp_connection_stream;
 //pub mod new_connection;
 mod one_second_metric;
 
@@ -8,13 +8,13 @@ pub mod read_loop;
 mod tcp_buffer_to_send;
 pub use tcp_buffer_to_send::*;
 mod tcp_connection_states;
-pub use connection::TcpSocketConnection;
 pub use connection_statistics::ConnectionStatistics;
-pub use connection_stream::*;
 pub use one_second_metric::OneSecondMetric;
 pub use tcp_buffer_to_send::TcpBufferToSend;
+pub use tcp_connection::TcpSocketConnection;
 pub use tcp_connection_states::TcpConnectionStates;
-mod connection_inner;
-pub use connection_inner::*;
+pub use tcp_connection_stream::*;
+mod tcp_connection_inner;
+pub use tcp_connection_inner::*;
 mod buffer_to_send_wrapper;
 pub use buffer_to_send_wrapper::*;
